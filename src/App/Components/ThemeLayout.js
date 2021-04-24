8f61b61feaa569bac739b606d602779ffa278571
shamshir anees <shamshir.anees@gmail.com>
2021-04-24 16:14:08 +0530

    fea: - change headers - auction detail countdown.

--- src/App/Components/ThemeLayout.js
+++ src/App/Components/ThemeLayout.js
@@ -1,43 +1,87 @@
-import React from 'react'
-import { Layout, Menu, Breadcrumb } from 'antd';
-import { Link } from 'react-router-dom';
-import { Colors } from '../Theme';
-
+import React from "react";
+import { Layout, Menu, Breadcrumb } from "antd";
+import { Link } from "react-router-dom";
+import { Colors } from "../Theme";
+import {
+  HomeFilled,
+  CalendarOutlined,
+  ProfileOutlined,
+  FolderAddOutlined,
+  AppstoreOutlined,
+  UserOutlined,
+  QuestionCircleOutlined,
+} from "@ant-design/icons";
 const { Header, Content, Footer } = Layout;
-export const ThemeLayout = (ComposedComponent, type = 'light') => (props) => {
+const { SubMenu } = Menu;
+export const ThemeLayout = (ComposedComponent, type = "light") => (props) => {
+  return (
+    <Layout className="layout">
+      <Header style={{ backgroundColor: Colors.white }}>
+        <div className="logo" />
+        <Menu theme="light" mode="horizontal">
+          <Menu.Item key="1" icon={<HomeFilled />}>
+            <Link to="/">Home</Link>
+          </Menu.Item>
+          <Menu.Item key="2" icon={<CalendarOutlined />}>
+            <Link to="/">Auctions</Link>
+          </Menu.Item>
+          <Menu.Item key="3" icon={<FolderAddOutlined />}>
+            <Link to="/createauction">Create</Link>
+          </Menu.Item>
+          <Menu.Item key="4" icon={<AppstoreOutlined />}>
+            <Link to="/category">Categories</Link>
+          </Menu.Item>
 
-    return (
-        <Layout className="layout">
-    <Header>
-      <div className="logo" />
-      <Menu theme="light" mode="horizontal" >
-        <Menu.Item  key="1"><Link  to="/">Home</Link></Menu.Item>
-        <Menu.Item key="2"><Link  to="/">All Auctions</Link></Menu.Item>
-        <Menu.Item key="3"><Link  to="/createauction">Create</Link></Menu.Item>
-        <Menu.Item key="4"><Link  to="/category">Categories</Link></Menu.Item>
+          <Menu.Item key="5" icon={<ProfileOutlined />}>
+            <Link to="/myBiddings">My Biddings</Link>
+          </Menu.Item>
+        </Menu>
+        <Menu
+          style={{ float: "right", marginTop: -66.5 }}
+          theme="light"
+          mode="horizontal"
+        >
+          <Menu.Item key="6" icon={<QuestionCircleOutlined />}>
+            <Link to="/">How It Works</Link>
+          </Menu.Item>
 
-        <Menu.Item key="5"><Link  to="/myBiddings">My Biddings</Link></Menu.Item>
+          <SubMenu key="7" title="Profile" icon={<UserOutlined />}>
+            <Menu.Item key="setting:1">View Profile</Menu.Item>
+            <Menu.Item key="setting:2">Sign Out</Menu.Item>
+          </SubMenu>
+        </Menu>
+      </Header>
+      <Content
+        style={{
+          maxWidth: 1230,
+          margin: "auto",
+          paddingRight: 15,
+          paddingLeft: 15,
+        }}
+      >
+        <Breadcrumb style={{ margin: "16px 0" }}>
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+          <Breadcrumb.Item>List</Breadcrumb.Item>
+          <Breadcrumb.Item>App</Breadcrumb.Item>
+        </Breadcrumb>
+        <div className="site-layout-content">
+          <ComposedComponent {...props} />
+        </div>
+      </Content>
+      <Footer
+        style={{
+          textAlign: "center",
+          backgroundColor: Colors.primary,
+          color: Colors.white,
+          height: 200,
+          float: "inline-end",
+          paddingTop: 170,
+        }}
+      >
+        Design ©2021 Created by Shamshir
+      </Footer>
+    </Layout>
+  );
+};
 
-      </Menu>
-    </Header>
-    <Content style={{maxWidth: 1230,
-    margin:'auto',
-    paddingRight: 15,
-    paddingLeft: 15 }}>
-      <Breadcrumb style={{ margin: '16px 0' }}>
-        <Breadcrumb.Item>Home</Breadcrumb.Item>
-        <Breadcrumb.Item>List</Breadcrumb.Item>
-        <Breadcrumb.Item>App</Breadcrumb.Item>
-      </Breadcrumb>
-      <div className="site-layout-content">
-
-
-      <ComposedComponent {...props} />
-      </div>
-    </Content>
-    <Footer style={{ textAlign: 'center',backgroundColor:Colors.primary,color:Colors.white,height:200,float:'inline-end',paddingTop:170 }}>Design ©2021 Created by Shamshir</Footer>
-  </Layout>
-    )  
-}
-
-export default ThemeLayout
+export default ThemeLayout;
--- src/App/Screens/ActionDetails/AuctionDetails.jsx
+++ src/App/Screens/ActionDetails/AuctionDetails.jsx
@@ -35,22 +35,23 @@ import { Link } from "react-router-dom";
 import { Colors } from "../../Theme";
 import Swiper from "react-id-swiper";
 import "swiper/css/swiper.css";
-import { Typography, Alert,Row, Col  } from "antd";
+import { Typography, Alert, Row, Col, Progress } from "antd";
 import { Statistic } from "antd";
-import { Timeline,notification } from 'antd';
-
+import { Timeline, notification } from "antd";
+import Countdown from "react-countdown";
 import {
   ShopOutlined,
   MailOutlined,
   PhoneOutlined,
   EnvironmentOutlined,
-  LoadingOutlined,CheckCircleTwoTone,
+  LoadingOutlined,
+  CheckCircleTwoTone,
   PlusCircleTwoTone,
   MinusCircleTwoTone,
 } from "@ant-design/icons";
 import ThemeLayout from "../../Components/ThemeLayout";
+import flag from "../../assets/Images/flagCheck.png";
 
-const { Countdown } = Statistic;
 const { Title } = Typography;
 const useStyles = makeStyles((theme) => ({
   icon: {
@@ -73,14 +74,14 @@ const StyledTableCell = withStyles((theme) => ({
 }))(TableCell);
 function AuctionDetails() {
   const openNotification = () => {
+    setdeadline(Date.now() + 20000)
     notification.open({
-
-      icon:<CheckCircleTwoTone twoToneColor={Colors.green} />,
-      message: 'Bid Placed',
+      icon: <CheckCircleTwoTone twoToneColor={Colors.green} />,
+      message: "Bid Placed",
       description:
-        'Your bid has been placed successfully. To claim the product, please wait until the bid time is over',
+        "Your bid has been placed successfully. To claim the product, please wait until the bid time is over",
       onClick: () => {
-        console.log('Notification Clicked!');
+        console.log("Notification Clicked!");
       },
     });
   };
@@ -113,6 +114,8 @@ function AuctionDetails() {
     "Los Angeles battles huge wildfires.",
   ];
   const [selectedImage, setSelectedImage] = useState(0);
+  const [progress, setProgress] = useState(10);
+  const [deadline, setdeadline] = useState(Date.now() + 20000);
   const classes = useStyles();
   const ref = useRef(null);
 
@@ -122,84 +125,87 @@ function AuctionDetails() {
       ref.current.swiper.slideTo(item);
     }
   };
-  const deadline = Date.now() + 1000 * 60 * 60 * 24 * 2 + 1000 * 30;
+  const bidTimesUp = (item) => {
+    setProgress(0);
+  };
+  const onCounterValueChange = (item) => {
+    console.log(item.seconds);
+
+    setProgress(item.seconds);
+    setdeadline(Date.now() + item.total);
+  };
+
   const [currentBid, setcurrentBid] = useState(635);
   return (
     <div>
-      <div style={{width:'100%',backgroundColor:'#e6f7ff'}}>
-      <Alert style={{width: "50%",margin: 'auto'}} message={
-       <span>Bidding starts <strong>Sat, Apr 24 at 6:29 AM</strong><a><strong> Set reminder</strong></a></span>
-      }
-        banner type="info" showIcon />
+      <div style={{ width: "100%", backgroundColor: "#e6f7ff" }}>
+        <Alert
+          style={{ width: "50%", margin: "auto" }}
+          message={
+            <span>
+              Bidding starts <strong>Sat, Apr 24 at 6:29 AM</strong>
+              <a>
+                <strong> Set reminder</strong>
+              </a>
+            </span>
+          }
+          banner
+          type="info"
+          showIcon
+        />
       </div>
-      {/* <div
-        style={{
-          height: 41,
-          backgroundColor: "#E9F6FE",
-          textAlign: "center",
-          fontSize: 14,
-          paddingTop: 5,
-        }}
+
+      <Row
+        gutter={{ xs: 16, sm: 16, md: 24, lg: 32 }}
+        style={{ marginTop: 30 }}
       >
-        <Info color="primary" />
-        This vehicle is in an auction that starts Thu, Apr 22 at 7:30 PM.
-        <Button href="#text-buttons" color="primary" style={{ marginTop: -3 }}>
-          Set reminder
-        </Button>
-      </div> */}
-
-<Row gutter={{ xs: 16, sm: 16, md: 24, lg: 32 }} style={{marginTop:30}}>
-      <Col className="gutter-row" span={16}>
-      <Title>Cashier Station - 96"x 32"x 42"</Title>
-      <Card>
-              <Swiper ref={ref} {...params}>
-                {[0,1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13].map((item) => (
-                  <img
+        <Col className="gutter-row" span={16}>
+          <Title>Cashier Station - 96"x 32"x 42"</Title>
+          <Card>
+            <Swiper ref={ref} {...params}>
+              {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13].map((item) => (
+                <img
                   src={`https://source.unsplash.com/random/300x200?sig=${item}`}
+                  alt=""
+                />
+              ))}
+            </Swiper>
+          </Card>
 
-                    alt=""
-                  />
-                ))}
-              </Swiper>
-            </Card>
+          <Row gutter={[16, 16]} style={{ marginTop: 20 }}>
+            {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13].map((item) => (
+              <Col span={3}>
+                <div
+                  onClick={() => goToImage(item)}
+                  style={{
+                    height: 80,
+                    width: 80,
+                    overflow: "hidden",
+                    borderRadius: 5,
+                    border:
+                      selectedImage === item
+                        ? "3px solid green"
+                        : "0px solid green",
 
-      
-            <Row gutter={[16, 16]} style={{marginTop:20}}>
-  
-  {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13].map((item) => (
-    <Col span={3} >
-                  <div
-                    onClick={() => goToImage(item)}
+                    marginRight: 10,
+                    marginBottom: 5,
+                  }}
+                >
+                  <img
                     style={{
                       height: 80,
                       width: 80,
-                      overflow: 'hidden',
                       borderRadius: 5,
-                      border:
-                        selectedImage === item
-                          ? "3px solid green"
-                          : "0px solid green",
-
-                      marginRight: 10,
-                      marginBottom: 5,
-                     }}
-                  >
-                    <img
-                      style={{
-                        height: 80,
-                        width: 80,
-                        borderRadius: 5,
-                      }}
-                      src={`https://source.unsplash.com/random/300x200?sig=${item}`}
-                    />
-                  </div>
-                  </Col>
-                ))}
-  
-  </Row>
-  <Row style={{marginTop:20}}>
-    <Col >
-  <Title level={4}>Seller Info</Title>
+                    }}
+                    src={`https://source.unsplash.com/random/300x200?sig=${item}`}
+                  />
+                </div>
+              </Col>
+            ))}
+          </Row>
+          <Row style={{ marginTop: 20 }}>
+            <Col>
+              <Title level={4}>Seller Info</Title>
               <List>
                 {[
                   {
@@ -222,141 +228,162 @@ function AuctionDetails() {
                   </ListItem>
                 ))}
               </List>
-              </Col>
-            
-  </Row>
-      </Col>
-      
-      <Col  className="gutter-row" span={8}>
+            </Col>
+          </Row>
+        </Col>
 
-      <Card style={{ textAlign: "center", paddingTop: 20 }}>
-              <div className="bid-amount">$ 625</div>
-              <div style={{ fontSize: 14, marginBottom: 20 }}>
-                Current Bid (97 bids)
-              </div>
-              <Divider />
-
-              <div
-                style={{
-                  display: "flex",
-                  justifyContent: "center",
-                  marginTop: 20,
-                }}
-              >
-                <MinusCircleTwoTone
-                  twoToneColor={currentBid === 635 ? "#ddd" : Colors.secondary}
-                  style={{ fontSize: 40 }}
-                  onClick={() => setcurrentBid(currentBid - 15)}
-                />
-                <Title
-                  style={{
-                    marginRight: 20,
-                    marginLeft: 20,
-                    marginTop: 5,
-                    color: Colors.lightGrey,
-                  }}
-                  level={2}
-                >
-                  $ {currentBid}
-                </Title>
-                <PlusCircleTwoTone
-                  onClick={() => setcurrentBid(currentBid + 15)}
-                  twoToneColor={Colors.secondary}
-                  style={{ fontSize: 40, color: Colors.secondary }}
-                />
-              </div>
-              <Button
-              onClick={()=>openNotification()}
-                variant="contained"
-                color="secondary"
-                style={{ margin: 20, color: Colors.white }}
-              >
-                Place Bid
-              </Button>
-              <Divider />
-              <CardContent className={classes.cardContent}>
-                <Table className={classes.table} aria-label="customized table">
-                  <TableBody>
-                    {data.map((row) => (
-                      <TableRow key={row.name}>
-                        <StyledTableCell component="th" scope="row">
-                          {row.name}
-                        </StyledTableCell>
-                        <TableCell align="right">{row.value}</TableCell>
-                      </TableRow>
-                    ))}
-                  </TableBody>
-                </Table>
-
-                <div style={{ display: "flex", justifyContent: "flex-start" }}>
-                  <Countdown
-                    title="Time Left"
-                    value={deadline}
-                    format="HH:mm:ss"
-                  />
-                  <Timeline style={{ marginTop:20 }}>
-                  {[0, 1, 2, 3, 4].map((item) => ( <Timeline.Item><strong>$17,350</strong> by Bidder{" "}
-                          <em class="timestamp">4 minutes ago</em></Timeline.Item>))}
-   
-  </Timeline>
-                
-                </div>
-              </CardContent>
+        <Col className="gutter-row" span={8}>
+          <Card style={{ textAlign: "center", paddingTop: 20 }}>
+            {progress === 0 ? (
               <div>
+                <img src={flag} style={{ height: 150, width: 150 }} />
+                <div className="bid-amount">SOLD $ 625</div>
+              </div>
+            ) : (
+              <div>
+                <div className="bid-amount">$ 625</div>
+                <div style={{ fontSize: 14, marginBottom: 20 }}>
+                  Current Bid (97 bids)
+                </div>
+                <Divider />
+
+                <Countdown
+                  className="ant-statistic-content"
+                  onComplete={bidTimesUp}
+                  daysInHours={true}
+                  date={deadline}
+                  onTick={(s) => onCounterValueChange(s)}
+                />
+
+                {progress < 10 ? (
+                  <Progress
+                    percent={10 - progress === 0 ? 100 : (10 - progress) * 10}
+                    size="small"
+                    status="exception"
+                    showInfo={false}
+                  />
+                ) : (
+                  <Divider />
+                )}
+
                 <div
                   style={{
-                    backgroundColor: "#C5E8C1",
-                    height: 53,
-                    flexDirection: "row",
-                    justifyContent: "space-between",
                     display: "flex",
+                    justifyContent: "center",
+                    marginTop: 20,
                   }}
                 >
+                  <MinusCircleTwoTone
+                    twoToneColor={
+                      currentBid === 635 ? "#ddd" : Colors.secondary
+                    }
+                    style={{ fontSize: 40 }}
+                    onClick={() => setcurrentBid(currentBid - 15)}
+                  />
+                  <Title
+                    style={{
+                      marginRight: 20,
+                      marginLeft: 20,
+                      marginTop: 5,
+                      color: Colors.lightGrey,
+                    }}
+                    level={2}
+                  >
+                    $ {currentBid}
+                  </Title>
+                  <PlusCircleTwoTone
+                    onClick={() => setcurrentBid(currentBid + 15)}
+                    twoToneColor={Colors.secondary}
+                    style={{ fontSize: 40, color: Colors.secondary }}
+                  />
+                </div>
+                <Button
+                  onClick={() => openNotification()}
+                  variant="contained"
+                  color="secondary"
+                  style={{ margin: 20, color: Colors.white }}
+                >
+                  Place Bid
+                </Button>
+              </div>
+            )}
+            <Divider />
+            <div style={{ display: "flex", justifyContent: "center" }}>
+              <Timeline style={{ marginTop: 35, marginBottom: -20 }}>
+                {[0, 1, 2, 3, 4].map((item) => (
+                  <Timeline.Item>
+                    <strong>$17,350</strong> by Bidder{" "}
+                    <em class="timestamp">4 minutes ago</em>
+                  </Timeline.Item>
+                ))}
+              </Timeline>
+            </div>
+            <Divider />
+
+            <CardContent className={classes.cardContent}>
+              <Table className={classes.table} aria-label="customized table">
+                <TableBody>
+                  {data.map((row) => (
+                    <TableRow key={row.name}>
+                      <StyledTableCell component="th" scope="row">
+                        {row.name}
+                      </StyledTableCell>
+                      <TableCell align="right">{row.value}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </CardContent>
+            {/* <div>
+              <div
+                style={{
+                  backgroundColor: "#C5E8C1",
+                  height: 53,
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                  display: "flex",
+                }}
+              >
+                <div
+                  style={{
+                    backgroundColor: "#395836",
+                    height: 53,
+                    width: 60,
+                    fontSize: 14,
+                    fontWeight: "bold",
+                    textAlign: "center",
+                    color: Colors.white,
+                  }}
+                >
+                  <div style={{ marginTop: 17 }}>BID</div>
+                  <div className={"auction-anchor"}></div>
+                </div>
+                <div style={{ marginRight: 10 }}>
                   <div
                     style={{
-                      backgroundColor: "#395836",
-                      height: 53,
-                      width: 60,
                       fontSize: 14,
                       fontWeight: "bold",
-                      textAlign: "center",
-                      color: Colors.white,
+                      color: "#555",
                     }}
                   >
-                    <div style={{ marginTop: 17 }}>BID</div>
-                    <div className={"auction-anchor"}></div>
+                    Current Bid
                   </div>
-                  <div style={{ marginRight: 10 }}>
-                    <div
-                      style={{
-                        fontSize: 14,
-                        fontWeight: "bold",
-                        color: "#555",
-                      }}
-                    >
-                      Current Bid
-                    </div>
-                    <div
-                      style={{
-                        fontSize: 18,
-                        fontWeight: "bold",
-                        textAlign: "right",
-                      }}
-                    >
-                      50.00
-                    </div>
+                  <div
+                    style={{
+                      fontSize: 18,
+                      fontWeight: "bold",
+                      textAlign: "right",
+                    }}
+                  >
+                    50.00
                   </div>
                 </div>
               </div>
-            </Card>
-          
-      </Col>
-    </Row>
-
-
-
-     </div>
-     
+            </div> */}
+          </Card>
+        </Col>
+      </Row>
+    </div>
   );
 }
 
