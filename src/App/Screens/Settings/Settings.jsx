ae4c15fefe259af59b3214ee499745571f34742d
Haridasan <haridasan.shanmugam@networkredux.com>
2021-05-14 22:17:49 +0530

    Settings Page will now show the static values and edit can be done if it's necessary

--- src/App/Screens/Settings/Settings.jsx
+++ src/App/Screens/Settings/Settings.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import { useDispatch,useSelector } from 'react-redux'
 import { getSettings, updateSettings } from '../../Redux/Actions/settings'
 import { Container, Typography,FormControl, TextField, Button, MenuItem } from '@material-ui/core'
+import EditIcon from '@material-ui/icons/Edit';
 
 const Settings = () => {
   const dispatch = useDispatch()
@@ -15,6 +16,8 @@ const Settings = () => {
     Currency:"$",
     Logo:""
   })
+
+  const [showForm, setShowForm] = useState(false)
 
   useEffect(() => {
     if(allSettings){
@@ -63,26 +66,43 @@ const Settings = () => {
     <Fragment>
       <Container maxWidth="sm">
         <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
-            Settings
+            Settings <a href="#" onClick={e => {
+              e.preventDefault()
+              setShowForm(!showForm)
+            }}><EditIcon /></a>
         </Typography>
-        <form className="settings" noValidate autoComplete="off" onSubmit={e => formHandle(e)}>
-          <FormControl margin="dense" fullWidth>
-            <TextField label="Display Name" defaultValue={DisplayName} name="DisplayName" onChange={handleChange} />
-          </FormControl>
-          <FormControl margin="dense" fullWidth>
-            <TextField select label="Currency" name="Currency" value={Currency} onChange={handleChange}>
-            {currencies.map((option) => (
-            <MenuItem key={option.value} value={option.value}>
-              {option.label}
-            </MenuItem>
-          ))}
-            </TextField>
-          </FormControl>
-          <FormControl margin="dense" fullWidth>
-            <TextField label="Logo URL" name="Logo" value={Logo} onChange={handleChange} />
-          </FormControl>
-          <Button variant="contained" type="submit" color="primary">Update</Button>
-        </form>
+        <div>
+        <Typography component="h3" variant="h3" align="center" color="textPrimary" gutterBottom>
+          Display Name: {DisplayName}
+        </Typography>
+        <Typography component="h3" variant="h3" align="center" color="textPrimary" gutterBottom>
+        Currency: {Currency}
+        </Typography>
+        <Typography component="h3" variant="h3" align="center" color="textPrimary" gutterBottom>
+          Logo: <br/>
+          <img src={Logo} alt="" />
+        </Typography>
+        </div>
+        <div style={showForm ? {display:"block"} : {display:"none"}}>
+          <form className="settings" noValidate autoComplete="off" onSubmit={e => formHandle(e)}>
+            <FormControl margin="dense" fullWidth>
+              <TextField label="Display Name" value={DisplayName} name="DisplayName" onChange={handleChange} />
+            </FormControl>
+            <FormControl margin="dense" fullWidth>
+              <TextField select label="Currency" name="Currency" value={Currency} onChange={handleChange}>
+              {currencies.map((option) => (
+              <MenuItem key={option.value} value={option.value}>
+                {option.label}
+              </MenuItem>
+            ))}
+              </TextField>
+            </FormControl>
+            <FormControl margin="dense" fullWidth>
+              <TextField label="Logo URL" name="Logo" value={Logo} onChange={handleChange} />
+            </FormControl>
+            <Button variant="contained" type="submit" color="primary">Update</Button>
+          </form>
+        </div>
       </Container>
     </Fragment>
   )
